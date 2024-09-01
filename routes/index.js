5f802c8bbfa0ff58c542fcf42f8fe0e9b2934eb2
brikhotso <brenda.rikhotso@gmail.com>
2024-09-01 15:03:36 -0700

    update routes

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,17 @@
-import express from 'express';
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
-const router = express.Router();
+const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
 router.post('/users', UsersController.postNew);
-router.get('/users/me', UsersController.getMe);
-
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-
+router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
@@ -22,4 +19,4 @@ router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 router.get('/files/:id/data', FilesController.getFile);
 
-export default router;
+module.exports = router;
